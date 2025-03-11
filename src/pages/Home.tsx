420c5640ccffbaaf9d8fd5a193617e0488176e21
Wizz0 <samgorokhov01@gmail.com>
2025-03-11 16:20:38 +0900

    fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import { Button } from '../components/Button'
 
 export default class Home extends Component {
     render() {
         return (
             <div>Home</div>
-
+            // <Button color="secondary" size="large" title="Подтвердить" />
+            // <Button color="secondary">
         )
     }
 }
